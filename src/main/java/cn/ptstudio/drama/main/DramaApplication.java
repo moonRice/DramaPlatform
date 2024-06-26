cfeb6a83d013a419fe3912e498c40623eb6f430b
sunRice <zhengyang1997@live.com>
2024-06-26 23:20:47 +0800

    初始化后端项目

---
+++ src/main/java/cn/ptstudio/drama/main/DramaApplication.java
@@ -0,0 +1,12 @@
+package cn.ptstudio.drama.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DramaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DramaApplication.class, args);
+    }
+
+}
